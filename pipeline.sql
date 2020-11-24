d53e968b4550ccd40dbbf8e690e65520e2e71de1
vector-client-token <>
2020-11-24 22:26:17 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -4,6 +4,15 @@
     Output(rid="ri.vector.main.execute.e971485e-0f1e-446b-99ad-68b2f21a8048"),
     Filterwithcodesetaliastable=Input(rid="ri.foundry.main.dataset.ff7e826a-1dbc-480e-86dc-d75aa802f9d8")
 )
-SELECT *
+SELECT count(distinct person_id) AS result, 'count_pers' as stat
+FROM Filterwithcodesetaliastable
+UNION ALL
+SELECT count(distinct visit_occurrence_id) AS result, 'count_visit' as stat
+FROM Filterwithcodesetaliastable
+UNION ALL
+SELECT count(distinct macrovisit_id) AS result, 'count_macro_v' as stat
+FROM Filterwithcodesetaliastable
+UNION ALL
+SELECT count(distinct alias) AS result, 'count_labs' as stat
 FROM Filterwithcodesetaliastable
 
