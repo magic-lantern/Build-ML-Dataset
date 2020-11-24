1d3c3e24b23fd6c8325d9110fe5d061606fb4dc8
vector-client-token <>
2020-11-24 22:18:27 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -1,2 +1,9 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.e971485e-0f1e-446b-99ad-68b2f21a8048"),
+    Filterwithcodesetaliastable=Input(rid="ri.foundry.main.dataset.ff7e826a-1dbc-480e-86dc-d75aa802f9d8")
+)
+SELECT *
+FROM Filterwithcodesetaliastable
+
