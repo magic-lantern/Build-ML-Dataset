a74f715c5b5379192423029b60725e5aeaa53a40
vector-client-token <>
2020-11-25 21:46:55 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,16 +1,3 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-@transform_pandas(
-    Output(rid="ri.foundry.main.dataset.96a61276-4442-4694-a0fd-7e5709c73dc9"),
-    labs_from_inpatient_visits=Input(rid="ri.foundry.main.dataset.9cf45dff-b77e-4e52-bd3d-2209004983a2")
-)
-def datetime_histogram(labs_from_inpatient_visits):
-    tempdf = labs_from_inpatient_visits.filter(labs_from_inpatient_visits.measurement_time.isNotNull())
-    df = tempdf.toPandas()
-    sns.countplot(df.measurement_time)
-    plt.show()
-    #pout.show()
-    #return None
-    #return labs_from_inpatient_visits.filter(labs_from_inpatient_visits.measurement_datetime.isNotNull()).limit(1000)
-
