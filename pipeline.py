1917ca358994a5a1075f8c13dc032bb54b8a1d7f
vector-client-token <>
2020-11-25 19:20:52 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -7,6 +7,6 @@ import seaborn as sns
 def datetime_histogram(labs_from_inpatient_visits):
     tempdf = labs_from_inpatient_visits.filter(labs_from_inpatient_visits.measurement_datetime.isNotNull()).limit(1000)
     df = tempdf.toPandas()
-    sns.histplot(df.measurement_datetime)
-    return None
+    #sns.histplot(df.measurement_datetime)
+    #return None
 
