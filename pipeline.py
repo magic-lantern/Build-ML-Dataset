6367fc3265a73ab5128fc12f93bafdfaadbb7e18
vector-client-token <>
2020-11-25 17:33:38 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,2 +1,9 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.c50c04da-8d5e-42bb-8990-6b0752ff4ca6"),
+    labs_from_inpatient_visits=Input(rid="ri.foundry.main.dataset.9cf45dff-b77e-4e52-bd3d-2209004983a2")
+)
+def unnamed_1(labs_from_inpatient_visits):
+    
+
