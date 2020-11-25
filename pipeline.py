43b7437034b4e8f25b3674dfbc96626cba9c479b
vector-client-token <>
2020-11-25 17:35:26 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -4,6 +4,7 @@
     Output(rid="ri.vector.main.execute.c50c04da-8d5e-42bb-8990-6b0752ff4ca6"),
     labs_from_inpatient_visits=Input(rid="ri.foundry.main.dataset.9cf45dff-b77e-4e52-bd3d-2209004983a2")
 )
-def unnamed_1(labs_from_inpatient_visits):
-    
+def datetime_histogram(labs_from_inpatient_visits):
+    import seaborn as sns
+    sns.histplot(labs_from_inpatient_visits.measurement_datetime)
 
