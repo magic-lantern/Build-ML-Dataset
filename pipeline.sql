79b2b27f34f43285d1b232309789fa87b8bccce2
vector-client-token <>
2020-11-25 22:32:13 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -31,7 +31,6 @@ SELECT *
 FROM bestVisitPossible
 WHERE 1 = 1
 AND visit_concept_name LIKE 'Inpatient%'
-AND visit_start_date <= visit_end_date
 
 @transform_pandas(
     Output(rid="ri.vector.main.execute.7088f128-6b0d-4f7f-accf-20153d6d1777"),
