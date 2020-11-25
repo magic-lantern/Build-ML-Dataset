e83f6e24f34acd2ed022cac12e5938a7b5961809
vector-client-token <>
2020-11-25 16:29:07 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -1,6 +1,20 @@
 
 
 @transform_pandas(
+    Output(rid="ri.foundry.main.dataset.58eea22d-1433-4072-952a-5ac6d69234dd"),
+    Filterwithcodesetaliastable=Input(rid="ri.foundry.main.dataset.ff7e826a-1dbc-480e-86dc-d75aa802f9d8")
+)
+SELECT count(1), 'num_null' as stat_col
+FROM Filterwithcodesetaliastable
+where measurement_datetime is NULL
+
+UNION
+
+SELECT count(1), 'num_1900' as stat_col
+FROM Filterwithcodesetaliastable
+where measurement_datetime = '1900-01-01T00:00:00.000Z'
+
+@transform_pandas(
     Output(rid="ri.foundry.main.dataset.bdcd1f3c-5c7e-4297-a45d-1ed1011fb591"),
     Filterwithcodesetaliastable=Input(rid="ri.foundry.main.dataset.ff7e826a-1dbc-480e-86dc-d75aa802f9d8")
 )
@@ -30,18 +44,4 @@ FROM Filterwithcodesetaliastable
 UNION ALL
 SELECT count(distinct Alias) AS result, 'count_labs' as stat
 FROM Filterwithcodesetaliastable
-
-@transform_pandas(
-    Output(rid="ri.vector.main.execute.cbb1cc45-8783-46a8-9a45-0ac7dd58cb5f"),
-    Filterwithcodesetaliastable=Input(rid="ri.foundry.main.dataset.ff7e826a-1dbc-480e-86dc-d75aa802f9d8")
-)
-SELECT count(1), 'num_null' as stat_col
-FROM Filterwithcodesetaliastable
-where measurement_datetime is NULL
-
-UNION
-
-SELECT count(1), 'num_1900' as stat_col
-FROM Filterwithcodesetaliastable
-where measurement_datetime = '1900-01-01T00:00:00.000Z'
 
