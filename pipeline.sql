10e0c71383ed2d01bcdbaf8989d233dd5536ad1c
vector-client-token <>
2020-11-25 16:29:42 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -4,13 +4,13 @@
     Output(rid="ri.foundry.main.dataset.58eea22d-1433-4072-952a-5ac6d69234dd"),
     Filterwithcodesetaliastable=Input(rid="ri.foundry.main.dataset.ff7e826a-1dbc-480e-86dc-d75aa802f9d8")
 )
-SELECT count(1), 'num_null' as stat_col
+SELECT count(1) as result, 'num_null' as stat_col
 FROM Filterwithcodesetaliastable
 where measurement_datetime is NULL
 
 UNION
 
-SELECT count(1), 'num_1900' as stat_col
+SELECT count(1) as result, 'num_1900' as stat_col
 FROM Filterwithcodesetaliastable
 where measurement_datetime = '1900-01-01T00:00:00.000Z'
 
