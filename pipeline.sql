c6fb63cf2c081e602c461150b522de5aa564a7f7
vector-client-token <>
2020-11-25 16:56:36 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -51,6 +51,7 @@ where measurement_datetime = '1900-01-01T00:00:00.000Z'
     Output(rid="ri.vector.main.execute.cc4066b9-d5c1-4cd5-af67-8b9f25e96d65"),
     inpatient_bestVisitPossible=Input(rid="ri.foundry.main.dataset.2ae94403-e46c-4586-9863-470e06737fcc")
 )
-SELECT *
+SELECT count(1)
 FROM inpatient_bestVisitPossible
+WHERE visit_start_datetime IS NOT NULL
 
