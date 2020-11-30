b6683ddb63a53bfbe0aa6f3f227f949040a1816c
vector-client-token <>
2020-11-30 21:15:44 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -1,2 +1,10 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.82f45cb2-5388-4cb9-9c09-011ef18c77db"),
+    Death_unique_persons=Input(rid="ri.foundry.main.dataset.2b2e338d-728b-4878-9c8a-05268426926f")
+)
+unnamed_1 <- function(Death_unique_persons) {
+    
+}
+
