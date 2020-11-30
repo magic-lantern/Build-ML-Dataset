48c537b2e7208ffa9718f06cf0f0a8236fb25f54
vector-client-token <>
2020-11-30 19:43:25 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -24,11 +24,8 @@ def inpatient_payer( map2_visit_occurrence_payer_plan, inpatients):
     df = df.withColumn("payer_concept_name", F.lower(col("payer_concept_name")))
 
     #Pivot by payer_concept_name and cast each new column to a boolean
-    keep_col = ['visit_occurrence_id', 'person_id', 'payer_plan_period_start_date', 'payer_plan_period_end_date', 'data_partner_id', 'visit_start_date', 'visit_end_date']
-    pivot_df = df.groupby(keep_col).pivot("payer_concept_name").count()
-    bool_col = [c for c in pivot_df.columns if c not in keep_col]
-    for c in bool_col:
-        pivot_df = pivot_df.withColumn(c, F.col(c).cast('boolean'))
+    pivot_df = df.groupby('visit_occurrence_id').pivot("payer_concept_name").count()
+    pivot_df.select(*[col(c) if c == 'visit_occurrence_id' else col(c).cast('boolean') for c in pivot_df.columns])
     pivot_df = pivot_df.drop("null")
     
     return pivot_df
