915dd5253885989225c562bbd1405348a52f1c8b
vector-client-token <>
2020-11-30 17:36:56 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,3 +1,10 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.f7d52136-b6d3-4dcf-92c4-40ea90053277"),
+    inpatient_payer_join=Input(rid="ri.foundry.main.dataset.ea22427f-b454-417f-a608-9ced2a96bf77")
+)
+def unnamed_1(inpatient_payer_join):
+    
+
