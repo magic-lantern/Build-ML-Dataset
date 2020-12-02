d86716d5f136e04691ad16aa98beb8da8ed7f332
vector-client-token <>
2020-12-02 00:46:26 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -251,6 +251,13 @@ FROM inpatient_bestVisitPossible
 WHERE visit_start_datetime IS NOT NULL
 
 @transform_pandas(
+    Output(rid="ri.vector.main.execute.09f40f23-0dad-4d7d-bf5a-9f8116f618bc"),
+    inpatients=Input(rid="ri.foundry.main.dataset.a773e078-3908-4189-83a2-2831a8f002f9")
+)
+SELECT *
+FROM inpatients
+
+@transform_pandas(
     Output(rid="ri.foundry.main.dataset.8b112ce6-7e66-4752-b95a-bb17b1a64791"),
     bestVisitPossible=Input(rid="ri.foundry.main.dataset.18864c06-114d-428e-8be9-170ebdc97729")
 )
