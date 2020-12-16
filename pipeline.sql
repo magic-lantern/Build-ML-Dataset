3d8d9b394632e820b9598d66ffb7c6635d24909b
vector-client-token <>
2020-12-16 18:41:28 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -149,7 +149,7 @@ SELECT
         ELSE FALSE
         END AS Stroke
 FROM Pivot_on_charlson c
-RIGHT JOIN inpatients v
+INNER JOIN inpatients v
     ON c.person_id = v.person_id
 
 @transform_pandas(
