85575ebd6c30fdd27f8185b0f36dd7c92ae5133a
vector-client-token <>
2020-12-18 16:23:56 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -158,6 +158,7 @@ def inpatient_encoded_w_imputation(inpatient_encoded):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.07927bca-b175-4775-9c55-a371af481cc1"),
+    inpatient_charlson2=Input(rid="ri.foundry.main.dataset.ddd8560b-e059-42dc-89ed-1ad850bfcc82"),
     inpatient_payer=Input(rid="ri.foundry.main.dataset.d30362c9-a90a-4486-aba9-d67e40c25fd0"),
     inpatient_worst_labs=Input(rid="ri.foundry.main.dataset.c1c6e3b9-83ff-421a-b5c6-75518beec801"),
     inpatients=Input(rid="ri.foundry.main.dataset.a773e078-3908-4189-83a2-2831a8f002f9")
