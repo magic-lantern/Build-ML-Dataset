f5a683eaebdc14906e0747928cabe77f97c034b2
vector-client-token <>
2021-04-07 21:45:03 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -158,18 +158,18 @@ def inpatient_encoded_w_imputation(inpatient_encoded):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.07927bca-b175-4775-9c55-a371af481cc1"),
-    Missing_regions_replaced=Input(rid="ri.foundry.main.dataset.2935a4c3-66c4-422c-b87c-5f0851c2462b"),
     inpatient_charlson2=Input(rid="ri.foundry.main.dataset.ddd8560b-e059-42dc-89ed-1ad850bfcc82"),
     inpatient_payer=Input(rid="ri.foundry.main.dataset.d30362c9-a90a-4486-aba9-d67e40c25fd0"),
     inpatient_worst_labs=Input(rid="ri.foundry.main.dataset.c1c6e3b9-83ff-421a-b5c6-75518beec801"),
-    inpatients=Input(rid="ri.foundry.main.dataset.a773e078-3908-4189-83a2-2831a8f002f9")
+    inpatients=Input(rid="ri.foundry.main.dataset.a773e078-3908-4189-83a2-2831a8f002f9"),
+    person_region=Input(rid="ri.foundry.main.dataset.4275c731-8425-403c-a38e-f40e0b462b65")
 )
-def inpatient_ml_dataset(inpatients, inpatient_charlson2, inpatient_worst_labs, inpatient_payer, Missing_regions_replaced):
+def inpatient_ml_dataset(inpatients, inpatient_charlson2, inpatient_worst_labs, inpatient_payer, person_region):
     df = inpatients
     cdf = inpatient_charlson2
     ldf = inpatient_worst_labs
     pdf = inpatient_payer
-    rdf = Missing_regions_replaced
+    rdf = person_region
     
     cdf = cdf.withColumnRenamed('person_id', 'p_id')
     ldf = ldf.withColumnRenamed('visit_occurrence_id', 'v_id')
